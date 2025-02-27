f86416cc1bd4236dd7a21872a894d27079fae6bb
mitjamob <107836807+mitjamob@users.noreply.github.com>
2025-02-27 16:52:27 +0100

    Update Header.tsx
    logo changed

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { ConnectWalletButton } from '../../features/wallet/ConnectWalletButton';
-import Logo from '../../images/logos/app-logo.svg';
+import Logo from '../../images/logos/everyone.svg';
 import Name from '../../images/logos/app-name.svg';
 import Title from '../../images/logos/app-title.svg';
 
