3bab815b104d358b67456a5ffced63b2b1d1ed89
mitjamob <107836807+mitjamob@users.noreply.github.com>
2025-02-27 16:02:43 +0100

    Update app.ts
    URL changed

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -9,7 +9,7 @@ export const MAIN_FONT = SpaceGrotesk({
 });
 export const APP_NAME = 'Custom Mitja Bridge';
 export const APP_DESCRIPTION = 'A DApp for Hyperlane Warp Route transfers';
-export const APP_URL = 'hyperlane-warp-template.vercel.app';
+export const APP_URL = 'hyperlane-custom-bridge.vercel.app';
 export const BRAND_COLOR = Color.primary['500'];
 export const BACKGROUND_COLOR = Color.primary['400'];
 export const BACKGROUND_IMAGE = 'url(/backgrounds/main.svg)';
