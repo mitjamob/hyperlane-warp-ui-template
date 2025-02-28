a5a4e3b1e2afc9ab3d44e0a0ee587321f5be99a2
mitjamob <107836807+mitjamob@users.noreply.github.com>
2025-02-28 09:51:22 +0100

    Update Header.tsx

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { ConnectWalletButton } from '../../features/wallet/ConnectWalletButton';
 import Logo from '../../images/logos/everyone.svg';
-import Name from '../../images/logos/app-name.svg';
-import Title from '../../images/logos/app-title.svg';
+import Name from '../../images/logos/everyone.svg';
+import Title from '../../images/logos/everyone.svg';
 
 export function Header() {
   return (
